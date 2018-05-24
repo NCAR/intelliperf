d193dbfb20ca3f8724a36e27ea8aa8fbb89445b3
Youngsung Kim <youngsun@ucar.edu>
2018-05-24 08:43:38 -0600

    added mg2 folding data

---
+++ exercise/data_handling/read_folding_csv.py
@@ -0,0 +1,3 @@
+# TODO: read "intelliperf/data/mg2/mg2.slope.csv"
+# TODO: generate a matplotlib line plot of a particular hardware counter data
+# TODO: save the plot as an image file in a file system.

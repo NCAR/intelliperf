c922722d805d50f98a5a7fb41cac3e3a9de12270
Youngsung Kim <youngsun@ucar.edu>
2018-06-05 10:07:21 -0600

    removed ilrelevant codes in pdfpages.py

--- exercise/plotting/pdfpages.py
+++ exercise/plotting/pdfpages.py
@@ -1,14 +1,10 @@
 import sys
 import numpy as np
-import dateutil
 
 try:
     from  matplotlib import pyplot as plt
-    from  matplotlib import colors as mcolors
-    from  matplotlib import text as figtext
     from matplotlib.backends.backend_pdf import PdfPages
 
-    colors = { idx:cname for idx, cname in enumerate(mcolors.cnames) }
     pdf = PdfPages('blank.pdf')
 except Exception as e:
     print ('ERROR: matplotlib module is not loaded: %s'%str(e))
@@ -31,18 +27,18 @@ def main():
 
     x = np.linspace(0, 1, num=num_points, endpoint=True)
 
-    # create RF
     for idx in range(num_pages):
 
         y = np.random.random(num_points)*y_range
 
-        ax.plot(x, y, alpha=0.4, color='b', linewidth=LINEWIDTH)
+        ax.scatter(x, y, alpha=0.4, color='b', linewidth=LINEWIDTH)
+
         ax.set_title("Title %d"%idx, fontsize=TITLE_SIZE)
-        yticks = [ t for t in np.linspace(0, y_range, num=num_yticks, endpoint=True)]
-        ax.set_yticks(yticks, ['{0:5.3f}'.format(ytick) for ytick in yticks])
+        yticks = np.linspace(0, y_range, num=num_yticks, endpoint=True)
+        ytick_labels = ['{0:5.3f}'.format(ytick) for ytick in yticks]
+        ax.set_yticks(yticks, ytick_labels)
         ax.set_xlabel("Samples", fontsize=LABEL_SIZE)
         ax.set_ylabel("Random value", fontsize=LABEL_SIZE)
-        ax.grid(True, axis='both', which='both')
 
         pdf.savefig()
         plt.cla()
